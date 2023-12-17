a0b5f16e2f7dcf5512a63a97abb460764415365c
chun1 <arthur@chunai.dev>
2023-12-17 23:55:35 +1100

    thumbnail component

--- src/thumbnail.tsx
+++ src/thumbnail.tsx
@@ -1,78 +1,66 @@
-import { useState } from 'react';
-import { RiArrowDownLine, RiArrowUpLine, RiEyeCloseLine, RiSaveLine } from 'react-icons/ri';
-
+import { ArrowDown, ArrowUp, EyeOff, Save } from 'solid-icons/hi';
 const ThumbnailButtonMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = createSignal(false);
 
   const handleToggleMenu = () => {
-    setIsOpen(!isOpen);
+    setIsOpen(!isOpen());
   };
-
   const handleOptionClick = (option) => {
     console.log(`Selected option: ${option}`);
+    // Perform the desired action based on the selected option
+    // Add your custom logic here
   };
 
   return (
-    <div className="relative inline-block text-left">
+    <div class="relative inline-block text-left">
       <div>
         <button
           type="button"
-          className="flex items-center justify-center w-8 h-8 rounded-full bg-gray-200 hover:bg-gray-300 focus:outline-none"
+          class="flex items-center justify-center w-8 h-8 rounded-full bg-gray-200 hover:bg-gray-300 focus:outline-none"
           onClick={handleToggleMenu}
         >
-          <svg
-            className="w-5 h-5 text-gray-600"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 3a2 2 0 100 4 2 2 0 000-4zm0 10a2 2 0 100 4 2 2 0 000-4zm0-5a2 2 0 100 4 2 2 0 000-4z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <Save class="w-5 h-5 text-gray-600" />
         </button>
       </div>
 
-      {isOpen && (
-        <div className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1">
-            <button
-              type="button"
-              className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none"
-              onClick={() => handleOptionClick('Save')}
-            >
-              <RiSaveLine className="w-4 h-4 mr-2" />
-              Save
-            </button>
-            <button
-              type="button"
-              className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none"
-              onClick={() => handleOptionClick('Hide')}
-            >
-              <RiEyeCloseLine className="w-4 h-4 mr-2" />
-              Hide
-            </button>
-            <button
-              type="button"
-              className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none"
-              onClick={() => handleOptionClick('Upvote')}
-            >
-              <RiArrowUpLine className="w-4 h-4 mr-2" />
-              Upvote
-            </button>
-            <button
-              type="button"
-              className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none"
-              onClick={() => handleOptionClick('Downvote')}
-            >
-              <RiArrowDownLine className="w-4 h-4 mr-2" />
-              Downvote
-            </button>
-          </div>
+      {/* {isOpen() && ( */}
+      <div class="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div class="py-1">
+          <button
+            type="button"
+            class="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none"
+            onClick={() => handleOptionClick('Save')}
+          >
+            <Save class="w-4 h-4 mr-2" />
+            Save
+          </button>
+          <button
+            type="button"
+            class="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none"
+            onClick={() => handleOptionClick('Hide')}
+          >
+            <EyeOff class="w-4 h-4 mr-2" />
+            Hide
+          </button>
+          <button
+            type="button"
+            class="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none"
+            onClick={() => handleOptionClick('Upvote')}
+          >
+            <ArrowUp class="w-4 h-4 mr-2" />
+            Upvote
+          </button>
+          <button
+            type="button"
+            class="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none"
+            onClick={() => handleOptionClick('Downvote')}
+          >
+            <ArrowDown class="w-4 h-4 mr-2" />
+            Downvote
+          </button>
         </div>
-      )}
+      </div>
+      {/* )} */}
     </div>
   );
 };
